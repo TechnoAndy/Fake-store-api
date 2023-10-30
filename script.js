9e9110439a4116809bfb60cdc05b2fcecb24c604
Andy <atdiegaardt@gmail.com>
2023-10-30 23:11:12 +0200

    Add to card function

--- script.js
+++ script.js
@@ -48,3 +48,45 @@ function createCard(){
   })
 }
 createCard();
+
+function addToCard(key){
+  if(cardList[key] ==null){
+    cardList[key] = JSON.parse(JSON.stringify(products[key]));
+    cardList[key].quantity = 1;
+    }
+    reloadCard();
+  }
+
+  function reloadCard(){
+    cardList.innerHTML = ``;
+    let count = 0;
+    let totalPrice = 0;
+    cardList.forEach((value, key)=>{
+      totalPrice = totalPrice + value.price;
+      count = count + value.quantity;
+      if(value != null){
+        let newDiv = document.createElement('li');
+        newDiv.innerHTML = `
+        <div><img src="image/${value.image}"/></div>
+        <div>${value.name}</div>
+        <div>${value.price.toLocaleString()}</div>
+        <div>
+          <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+          <div class="count">${value.quantity}</div>
+          <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+        </div>`;
+        listCard.appendChild(newDiv);
+      }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+  }
+  function changeQuantity(key, quantity){
+    if(quantity == 0){
+      delete cardList[key];
+    }else{
+      cardList[key].quantity = quantity;
+      cardList[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+  }
