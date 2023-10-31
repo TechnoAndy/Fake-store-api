a0c3e48bb33abfdd6e9823391b67495c1b5a4e1a
Andy <atdiegaardt@gmail.com>
2023-10-31 19:11:01 +0200

    Add instock function, items that have reached the stock limit will receive an alert message

--- script.js
+++ script.js
@@ -18,37 +18,43 @@ let products = [
     id: 1,
     name: 'Skater Maxi',
     image: '../images/1.png',
-    price: 850
+    price: 850,
+    instock: 2
   },
   {
     id: 2,
     name: 'Daska Maxi',
     image: '../images/2.png',
-    price: 580
+    price: 580,
+    instock: 5
   },
   {
     id: 3,
     name: 'Anarkali Maxi',
     image: '../images/3.png',
-    price: 1200
+    price: 1200,
+    instock: 25
   },
   {
     id: 4,
     name: 'Curved Maxi',
     image: '../images/4.png',
-    price: 900
+    price: 900,
+    instock: 10
   },
   {
     id: 5,
     name: 'Maya Petite',
     image: '../images/5.png',
-    price: 1500
+    price: 1500,
+    instock: 20
   },
   {
     id: 6,
     name: 'Satin Maxi',
     image: '../images/6.png',
-    price: 850
+    price: 850,
+    instock: 15
   },
 ];
 
@@ -69,13 +75,21 @@ function createCard(){
 }
 createCard();
 
-function addToCard(key){
-  if(cardList[key] == null){
-      cardList[key] = JSON.parse(JSON.stringify(products[key]));
-      cardList[key].quantity = 1;
+function addToCard(key) {
+  if (cardList[key] == null) {
+    cardList[key] = JSON.parse(JSON.stringify(products[key]));
+    cardList[key].quantity = 1;
+  } else {
+    // Check if quantity is within stock limit
+    if (cardList[key].quantity + 1 <= products[key].instock) {
+      cardList[key].quantity = cardList[key].quantity + 1;
+    } else {
+      alert("Sorry, we are out of stock");
     }
-    reloadCard();
   }
+  reloadCard();
+}
+
 
   function reloadCard() {
     listCard.innerHTML = ``;
